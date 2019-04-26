bd42bbfa50ac2aa6d7f310be482b7a2b794ead95
lilonglong <lilonglong.lll@bytedance.com>
2019-04-26 17:38:56 +0800

    feat: delete B
    Change-Id: I48b8e4e3ddfef56925b87d5f4574c52b8a6d0150

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -25,7 +25,7 @@ export default {
     return {
       localCount: 0,
       movies: this.$store.getters.singleMovies(3),
-      message1: 'ab',
+      message1: 'a',
       message2: '123'
     };
   },
